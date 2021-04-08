9acc7e8a3f6b501c32d2688cc65e0ec889dc4398
istrav <travis.burandt@gmail.com>
2021-04-08 04:02:54 -0500

    userData 4

--- istrav/index.ts
+++ istrav/index.ts
@@ -2,7 +2,8 @@
 import * as linode from "@pulumi/linode"
 import * as pulumi from "@pulumi/pulumi"
 
-const userData = `
+const debian9 = "linode/debian9"
+const startupScript = `
 #!/bin/bash
 sudo apt-get update
 sudo apt-get install -y nginx
@@ -25,6 +26,29 @@ echo "server {
 
 sudo service nginx restart
 `
+
+const profile = pulumi.output(linode.getProfile({ async: true }));
+
+const stackscript = new linode.StackScript("istrav", {
+  label: "istrav",
+  script: startupScript,
+  description: "nginx with a node.js/express API",
+  images: [debian9],
+});
+
+const linodeInstance = new linode.Instance(`istrav:::${pulumi.getStack()}`, {
+  type: "g6-nanode-1",
+  stackscriptId: stackscript.id,
+  image: debian9,
+  region: "us-east",
+  // Include all "LISH" registered SSH Keys
+  authorizedKeys: profile.authorizedKeys,
+  // Include all User configured SSH Keys
+  authorizedUsers: [profile.username],
+}, { dependsOn: [stackscript] });
+
+exports.instanceLabel = linodeInstance.label;
+exports.instanceIP = linodeInstance.ipAddress;
 
 const web = new linode.Instance(`istrav:::${pulumi.getStack()}`, {
   group: "foo",
