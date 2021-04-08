54a9f861307538d685dd0efe57a7a6551be7e845
istrav <travis.burandt@gmail.com>
2021-04-08 01:51:01 -0500

    name

--- istrav/index.ts
+++ istrav/index.ts
@@ -5,19 +5,18 @@ import * as pulumi from "@pulumi/pulumi"
 const dropletCount = 2
 const region = digitalocean.Regions.NYC3
 
-const dropletTypeTag = new digitalocean.Tag(`istrav-${pulumi.getStack()}`)
 const userData = `
   #!/bin/bash
   sudo apt-get update
   sudo apt-get install -y nginx`
 
-const nameTag = new digitalocean.Tag(`istrav:::istrav.com`)
-const droplet = new digitalocean.Droplet(`istrav:::istrav.com`, {
+const nameTag = new digitalocean.Tag(`istrav:::${pulumi.getStack()}`)
+const droplet = new digitalocean.Droplet(`istrav:::${pulumi.getStack()}`, {
   image: "ubuntu-18-04-x64",
   region: region,
   privateNetworking: true,
   size: digitalocean.DropletSlugs.DropletS1VCPU1GB,
-  tags: [nameTag.id, dropletTypeTag.id],
+  tags: [nameTag.id],
   userData: userData,
 })
 
