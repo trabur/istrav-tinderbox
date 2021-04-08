7d35f8a9cbba6a559d7f62ee108eb94397275b1d
istrav <travis.burandt@gmail.com>
2021-04-08 03:15:47 -0500

    userData

--- istrav/index.ts
+++ istrav/index.ts
@@ -8,7 +8,26 @@ const region = digitalocean.Regions.NYC3
 const userData = `
   #!/bin/bash
   sudo apt-get update
-  sudo apt-get install -y nginx`
+  sudo apt-get install -y nginx
+  sudo ufw allow 'Nginx HTTP'
+  sudo ufw enable
+  
+  echo "server {
+    listen       80;
+    server_name  localhost;
+  
+    location / {
+      proxy_pass http://localhost:3000;
+      proxy_http_version 1.1;
+      proxy_set_header Upgrade $http_upgrade;
+      proxy_set_header Connection 'upgrade';
+      proxy_set_header Host $host;
+      proxy_cache_bypass $http_upgrade;
+    }
+  }" >> '/etc/nginx/sites-available/default'
+
+  sudo service nginx restart
+  `
 
 const nameTag = new digitalocean.Tag(`istrav:::${pulumi.getStack()}`)
 const droplet = new digitalocean.Droplet(`istrav:::${pulumi.getStack()}`, {
