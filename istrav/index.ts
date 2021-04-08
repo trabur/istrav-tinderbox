54c3092a76869cc914ca27a1f9d378ba6f662e1f
istrav <travis.burandt@gmail.com>
2021-04-08 01:22:03 -0500

    droplet

--- istrav/index.ts
+++ istrav/index.ts
@@ -10,33 +10,15 @@ const userData = `
   #!/bin/bash
   sudo apt-get update
   sudo apt-get install -y nginx`
-const droplets = []
 
-for (let i = 0; i < dropletCount; i++) {
-  const nameTag = new digitalocean.Tag(`server-${i}`);
-  droplets.push(new digitalocean.Droplet(`server-${i}`, {
-    image: "ubuntu-18-04-x64",
-    region: region,
-    privateNetworking: true,
-    size: digitalocean.DropletSlugs.DropletS1VCPU1GB,
-    tags: [nameTag.id, dropletTypeTag.id],
-    userData: userData,
-  }))
-}
-
-const lb = new digitalocean.LoadBalancer("public", {
-  dropletTag: dropletTypeTag.name,
-  forwardingRules: [{
-    entryPort: 80,
-    entryProtocol: digitalocean.Protocols.HTTP,
-    targetPort: 80,
-    targetProtocol: digitalocean.Protocols.HTTP,
-  }],
-  healthcheck: {
-    port: 80,
-    protocol: digitalocean.Protocols.TCP,
-  },
+const nameTag = new digitalocean.Tag(`istrav:::istrav.com`)
+const droplet = new digitalocean.Droplet(`istrav:::istrav.com`, {
+  image: "ubuntu-18-04-x64",
   region: region,
+  privateNetworking: true,
+  size: digitalocean.DropletSlugs.DropletS1VCPU1GB,
+  tags: [nameTag.id, dropletTypeTag.id],
+  userData: userData,
 })
 
-export const endpoint = lb.ip
+export const ip = droplet.ipv4Address
