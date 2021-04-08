42712938acfd590c9642415ffb7491789c43e821
istrav <travis.burandt@gmail.com>
2021-04-08 16:25:30 -0500

    without ni

--- istrav/index.ts
+++ istrav/index.ts
@@ -35,35 +35,39 @@ echo "server {
 sudo service nginx restart
 `
 
-const myVpc = new aws.ec2.Vpc(`istrav-vpc:::${pulumi.getStack()}`, {
-  cidrBlock: "172.16.0.0/16",
-  tags: {
-    Name: "istrav",
-  },
-})
+// const myVpc = new aws.ec2.Vpc(`istrav-vpc:::${pulumi.getStack()}`, {
+//   cidrBlock: "172.16.0.0/16",
+//   tags: {
+//     Name: "istrav",
+//   },
+// })
 
-const mySubnet = new aws.ec2.Subnet(`istrav-subnet:::${pulumi.getStack()}`, {
-  vpcId: myVpc.id,
-  cidrBlock: "172.16.10.0/24",
-  availabilityZone: zone,
-  tags: {
-    Name: "istrav",
-  },
-})
+// const mySubnet = new aws.ec2.Subnet(`istrav-subnet:::${pulumi.getStack()}`, {
+//   vpcId: myVpc.id,
+//   cidrBlock: "172.16.10.0/24",
+//   availabilityZone: zone,
+//   tags: {
+//     Name: "istrav",
+//   },
+// })
 
-const fooNetworkInterface = new aws.ec2.NetworkInterface(`istrav-networkInterface:::${pulumi.getStack()}`, {
-  subnetId: mySubnet.id,
-  privateIps: ["172.16.10.100"],
-  tags: {
-    Name: "istrav",
-  },
-})
+// const fooNetworkInterface = new aws.ec2.NetworkInterface(`istrav-networkInterface:::${pulumi.getStack()}`, {
+//   subnetId: mySubnet.id,
+//   privateIps: ["172.16.10.100"],
+//   tags: {
+//     Name: "istrav",
+//   },
+// })
 
 const group = new aws.ec2.SecurityGroup("istrav-securityGroup:::${pulumi.getStack()}", {
   ingress: [
     { protocol: "tcp", fromPort: 22, toPort: 22, cidrBlocks: ["0.0.0.0/0"] },
     { protocol: "tcp", fromPort: 80, toPort: 80, cidrBlocks: ["0.0.0.0/0"] },
   ],
+  egress: [
+    { protocol: "tcp", fromPort: 22, toPort: 22, cidrBlocks: ["0.0.0.0/0"] },
+    { protocol: "tcp", fromPort: 80, toPort: 80, cidrBlocks: ["0.0.0.0/0"] },
+  ]
 })
 
 const fooInstance = new aws.ec2.Instance(`istrav-instance:::${pulumi.getStack()}`, {
@@ -71,10 +75,10 @@ const fooInstance = new aws.ec2.Instance(`istrav-instance:::${pulumi.getStack()}
   instanceType: instanceType,
   userData: startupScript,
   vpcSecurityGroupIds: [ group.id ],
-  networkInterfaces: [{
-    networkInterfaceId: fooNetworkInterface.id,
-    deviceIndex: 0,
-  }],
+  // networkInterfaces: [{
+  //   networkInterfaceId: fooNetworkInterface.id,
+  //   deviceIndex: 0,
+  // }],
   creditSpecification: {
     cpuCredits: "unlimited",
   },
